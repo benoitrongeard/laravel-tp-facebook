e05685fe2d7c2c9b9c4a0d0d71bfc677ddb6d488
Benoit Rongeard <benron@hotmail.fr>
2018-12-15 12:53:58 +0100

    :bug: Fix user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->integer('age');
-            $table->string('password')->nullable();
+            $table->string('password');
             $table->timestamps();
         });
     }
